d2eb6942725a1e0c98307283739a692310b06b99
S Soundarya <soundarya.s@tvsnext.com>
2023-07-17 10:49:18 +0530

    [Soundarya]Add: Add/Edit contact option.

--- AddressBook/AddressBook.cs
+++ AddressBook/AddressBook.cs
@@ -8,6 +8,7 @@ namespace AddressBook
 {
     public class AddressBook
     {
+        List<Contact> addressBook = new List<Contact>();
         public void CreateContact()
         {
             Console.WriteLine("Enter the details\n1.First Name\n2.Last Name\n3.Address\n4.city\n5.State\n6.Zip\n7.Phone Number\n8.Email");
@@ -23,6 +24,45 @@ namespace AddressBook
                 Email = Console.ReadLine(),
             };
             Console.WriteLine(contact.FirstName + "\n" + contact.LastName + "\n" + contact.Address + "\n" + contact.City + "\n" + contact.State + "\n" + contact.Zip + "\n" + contact.PhoneNumber + "\n" + contact.Email);
+            addressBook.Add(contact);
+        }
+        public void EditContact(string name)
+        {
+            foreach(var contact in addressBook)
+            {
+                if(contact.FirstName.Equals(name) || contact.LastName.Equals(name))
+                {
+                    Console.WriteLine("Enter the option to Edit\n1.First Name\n2.Last Name\n3.Address\n4.City\n5.State\n6.Zip\n7.Phone Number\n8.Email");
+                    int option = Convert.ToInt32(Console.ReadLine());
+                    switch (option)
+                    {
+                        case 1:
+                            contact.FirstName = Console.ReadLine();
+                            break;
+                        case 2:
+                            contact.LastName = Console.ReadLine();
+                            break;
+                        case 3:
+                            contact.Address = Console.ReadLine();
+                            break;
+                        case 4:
+                            contact.City = Console.ReadLine();
+                            break;
+                        case 5:
+                            contact.State = Console.ReadLine();
+                            break;
+                        case 6:
+                            contact.Zip = Console.ReadLine();
+                            break;
+                        case 7:
+                            contact.PhoneNumber = Console.ReadLine();
+                            break;
+                        case 8:
+                            contact.Email = Console.ReadLine();
+                            break;
+                    }
+                }
+            }
         }
        
     }
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -5,8 +5,28 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to AddresBook Problem ");
+            Console.WriteLine("Enter your option to proceed:\n1.Create Contact\n2.Edit Contact\n3.Exit ");
+            bool flag = true;
             AddressBook address = new AddressBook();
+            while (flag)
+            {
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        address.CreateContact();
+                            break;
+                    case 2:
+                        Console.WriteLine("Enter name to edit:");
+                        string name= Console.ReadLine();
+                        address.EditContact(name);
+                        break;
+                    case 3:
+                        flag = false;
+                        break;
+
+                }
+            }
             address.CreateContact();
         }
     }
