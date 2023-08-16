d7cd85c1bc5e6c118b5838bc6ab9dda2c9090fa1
Soundarya <67475352+soundarya12s@users.noreply.github.com>
2023-08-16 21:00:12 +0530

    Update AddressBook.cs

--- AddressBook/AddressBook.cs
+++ AddressBook/AddressBook.cs
@@ -12,6 +12,8 @@ namespace AddressBook
         Dictionary<string, List<Contact>> dict = new Dictionary<string, List<Contact>>();
         Dictionary<string, List<Contact>> City= new Dictionary<string, List<Contact>>();
         Dictionary<string, List<Contact>> State_ = new Dictionary<string, List<Contact>>();
+        
+        Dictionary<string, List<Contact>> sort = new Dictionary<string, List<Contact>>();
 
         public void CreateContact()
         {
